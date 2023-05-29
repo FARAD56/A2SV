b842e84df436803127f6ead4eece88dc86b0126e
Abubakari Al-Waasiu <alwaasiua@gmail.com>
2023-05-29 22:32:47 +0000

    Palindrome numbers

---
+++ Palindrome number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        y = x[::-1]
+        return x == y
