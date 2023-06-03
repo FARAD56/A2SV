250f2621de4c0fc21abbf65a2acd4a0ebf761546
Abubakari Al-Waasiu <alwaasiua@gmail.com>
2023-06-03 00:11:20 +0000

    nm

--- NestedList.py
+++ NestedList.py
@@ -1,21 +1,14 @@
-if __name__ == '__main__':
-    records = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        records.append([name,score])
+records = []
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    records.append([name,score])
 
-    records.sort(key = lambda x : x[1])
+grades = set(record[1] for record in records)
+second_lowest = sorted(grades)[1]
 
-    for elem in records:
-        if elem[1] == records[0][1]:
-            records.remove(elem)
+second_lowest_holder = [record[0] for record in records if record[1] == second_lowest]
 
-    print(records[1][0])
-    for elem in records:
-        if elem[1] == records[1][1]:
-            print(elem[0])
-
-
-        
-    
+second_lowest_holder.sort()
+for holder in second_lowest_holder:
+    print(holder)
