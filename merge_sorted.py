9b6f9a967e3935383a946a688847f4d74ffafa0f
Abubakari Al-Waasiu <alwaasiua@gmail.com>
2023-06-14 20:16:55 +0000

    mody

--- merge_sorted.py
+++ merge_sorted.py
@@ -1,44 +1,21 @@
-# def merge(nums1, m, nums2, n):
-#         """
-#         :type nums1: List[int]
-#         :type m: int
-#         :type nums2: List[int]
-#         :type n: int
-#         :rtype: None Do not return anything, modify nums1 in-place instead.
-#         """
-#         nums1 += nums2
-#         nums1 = nums1
-
-#         nums1 = [item for item in nums1 if item != 0]
-#         nums1.sort()
-
-#         return nums1
-
-# print(merge([0],0,[1],1))
-
-
 class Solution(object):
-   def merge(self,nums1, m, nums2, n):
+    def merge(self, nums1, m, nums2, n):
         """
         :type nums1: List[int]
         :type m: int
         :type nums2: List[int]
         :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        :rtype: None (modifies nums1 in-place)
         """
-        nums1.sort()
-        count = 0
-        for i in range(m):
-            if nums1[i] < 0:
-                nums2.append(nums1[i])
-                nums2.sort()
-
-
-        for i in range(len(nums2)):
-            if nums1[i] == 0:
-                nums1[i] = nums2[i] 
-                print(nums1)
-        nums1.sort()
-
-soln = Solution()
-soln.merge([-10,-10,-9,-9,-9,-8,-8,-7,-7,-7,-6,-6,-6,-6,-6,-6,-6,-5,-5,-5,-4,-4,-4,-3,-3,-2,-2,-1,-1,0,1,1,1,2,2,2,3,3,3,4,5,5,6,6,6,6,7,7,7,7,8,9,9,9,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],55,[-10,-10,-9,-9,-9,-9,-8,-8,-8,-8,-8,-7,-7,-7,-7,-7,-7,-7,-7,-6,-6,-6,-6,-5,-5,-5,-5,-5,-4,-4,-4,-4,-4,-3,-3,-3,-2,-2,-2,-2,-2,-2,-2,-1,-1,-1,0,0,0,0,0,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,7,7,7,7,7,7,7,8,8,8,8,9,9,9,9],99)
+        last_merged = m + n - 1
+        last_nums1 = m - 1
+        last_nums2 = n - 1
+        
+        while last_nums2 >= 0:
+            if last_nums1 >= 0 and nums1[last_nums1] > nums2[last_nums2]:
+                nums1[last_merged] = nums1[last_nums1]
+                last_nums1 -= 1
+            else:
+                nums1[last_merged] = nums2[last_nums2]
+                last_nums2 -= 1
+            last_merged -= 1
