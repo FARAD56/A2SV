db5d639346d566461d778ba9431493320a3db8b0
Abubakari Al-Waasiu <alwaasiua@gmail.com>
2023-06-15 01:28:58 +0000

    friend

--- findthefriend.py
+++ findthefriend.py
@@ -1,8 +1,14 @@
-n,k = int(input()),int(input())
-array = list(range(1,(n+1)))
-current = 0
-while len(array) > 1:
-    remove_index = (current + k -1) % len(array)
-    array.pop(remove_index)
-    current = remove_index % len(array)
-print(*array)
+class Solution(object):
+    def findTheWinner(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        array = list(range(1, n + 1)) 
+        current = 0 
+        while len(array) > 1:
+            idx = (current + k - 1) % len(array)
+            array.pop(idx)
+            current = idx % len(array)
+        return array[0]
