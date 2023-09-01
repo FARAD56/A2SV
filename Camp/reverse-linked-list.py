5ff962baadf5096418343306d3630bbf4b843320
Al-Waasiu Abubakari <99828747+FARAD56@users.noreply.github.com>
2023-09-01 11:35:50 +0000

    Added solution for Reverse Linked List

--- Camp/reverse-linked-list.py
+++ Camp/reverse-linked-list.py
@@ -1,20 +1,11 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        newnode = ListNode(-1)
         prev = None
         current = head
+        
         while current:
-            newnode.next = current.next
+            next = current.next
             current.next = prev
             prev = current
-            current = newnode.next
+            current = next
         return prev
