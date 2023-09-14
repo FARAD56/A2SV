5570ac467267e75fddfcca40dff675674e2a685f
Al-Waasiu Abubakari <99828747+FARAD56@users.noreply.github.com>
2023-09-14 16:53:10 +0000

    Added solution for Find Peak Element

--- Camp/find-peak-element.py
+++ Camp/find-peak-element.py
@@ -1,17 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        nums = [float("-inf")] + nums + [float("-inf")] 
-        l , r = 1, len(nums) -2
+        nums = nums + [float("-inf")] 
+        l , r = 0, len(nums) -2
         while l <= r:
             mid = (l + r)//2
             if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
-                return mid-1
+                return mid
             elif nums[mid+1] > nums[mid] and nums[mid+1] > nums[mid-1]:
                 l = mid + 1
             else:
                 r = mid -1
 
-        return l-1
+        return l
 
 
             
