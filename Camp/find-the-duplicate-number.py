9ae15b011957da56e1cb06d76745ff4977c558d5
Al-Waasiu Abubakari <99828747+FARAD56@users.noreply.github.com>
2023-09-22 16:06:50 +0000

    Added solution for Find the Duplicate Number

--- Camp/find-the-duplicate-number.py
+++ Camp/find-the-duplicate-number.py
@@ -1,7 +1,15 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
-        num = nums[0]
-        for num in nums:
-            if (bisect_right(nums,num) != bisect_left(nums,num) + 1):
-                return num
+        l, r = 0,len(nums)-1
+
+        while l <= r:
+            mid = l + (r-l)//2
+
+            count = sum([1 for num in nums if num <= mid])
+
+            if count > mid:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return l
