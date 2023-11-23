bbb0a96681ee0041f38df4cd959112a6a30b2c8c
Abubakari Al-Waasiu <alwaasiua@gmail.com>
2023-11-23 22:04:30 +0000

    quiz_amend

--- Progress/quiz1.py
+++ Progress/quiz1.py
@@ -39,3 +39,87 @@ else:
 # --------------------------------------
 
 
+# --------------------------------------
+# Function to convert Adjacency Matrix to Adjacency List
+def adjacency_matrix_to_adjacency_list(adj_matrix):
+    adj_list = {}
+
+    # Write your code here to convert the adjacency matrix to an adjacency list
+    # Your code goes here
+
+    return adj_list  # Return the adjacency list
+
+# Test case
+adj_matrix_sample = [
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [1, 0, 0, 1],
+    [0, 0, 0, 1]
+]
+
+# --- Student Section to Write Code ---
+from collections import defaultdict
+
+Dict = defaultdict(list)
+
+def adjacency_matrix_to_adjacency_list(adj_matrix_sample):
+    for src in range(len(adj_matrix_sample)):
+        for dest in range(len(adj_matrix_sample[0])):
+            if adj_matrix_sample[src][dest] == 1:
+                Dict[src].append(dest)
+    return Dict
+
+
+
+# Write your code for the conversion here
+result_adj_list = adjacency_matrix_to_adjacency_list(adj_matrix_sample)
+expected_result = {
+    0: [1, 2],
+    1: [2],
+    2: [0, 3],
+    3: [3]
+}
+
+# Print the result for verification
+print("Result:", result_adj_list)
+if result_adj_list == expected_result:
+    print("Correct! Your code produced the expected result.")
+else:
+    print("Incorrect! Please try again")
+# --------------------------------------
+
+
+
+from collections import defaultdict
+# --------------------------------------
+# Function to convert Edge List to Adjacency List
+def edge_list_to_adjacency_list(edge_list):
+
+	# Write your code here to convert the edge list to an adjacency list.
+    adj_list = defaultdict(list)
+
+    for src, dest in edge_list:
+        adj_list[src].append(dest)
+
+    return adj_list
+
+# Test case
+edge_list_sample = [(0, 1), (0, 2), (1, 2), (2, 0), (2, 3), (3, 3)]
+
+# --- Student Section to Write Code ---
+# Write your code for the conversion here
+result_adj_list = edge_list_to_adjacency_list(edge_list_sample)
+expected_result = {
+    0: [1, 2],
+    1: [2],
+    2: [0, 3],
+    3: [3]
+}
+
+# Print the result for verification
+print("Result:", result_adj_list)
+if result_adj_list == expected_result:
+    print("Correct! Your code produced the expected result.")
+else:
+    print("Incorrect! Please try again")
+# --------------------------------------
